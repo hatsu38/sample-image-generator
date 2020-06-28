6449992e593544a1be0d7485ec95dc50d2331e7b
hatsu38 <hajiwata0308@gmail.com>
2020-06-28 17:13:10 +0900

    Add Canvas Component

---
+++ src/components/canvasImage.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class CanvasImage extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  componentDidMount() {
+    this.updateCanvas();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props !== nextProps) {
+      this.updateCanvas();
+    }
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const { canvas } = this;
+    const context = canvas.getContext('2d');
+    console.log('context', context);
+    console.log('props', this.props);
+    this.props.updateCanvas(context);
+  }
+
+  render() {
+    return <canvas ref={(e) => { this.canvas = e; }} width={this.props.width} height={this.props.height} style={{backgroundColor: this.props.backgroundColor}}></canvas>;
+  }
+}
+
+// CanvasImage.propTypes = {
+//   width: PropTypes.number.isRequired,
+//   height: PropTypes.number.isRequired,
+//   backGroundColor: PropTypes.string.isRequired,
+//   updateCanvas: PropTypes.func.isRequired,
+// };
